366c6e4dcb297d8769312349600667e0ba16483e
Vrooty <victoriadohertybone@live.co.uk>
2023-11-02 13:38:34 +0000

    message

--- server/server.js
+++ server/server.js
@@ -6,6 +6,25 @@ const app = express();
 const axios = require("axios");
 
 
-app.get("/", (_, response) => response.json("Root route for translatim."));a
+app.get("/", async (_, response) => response.json("Root route for translatim."));
+
+app.get("/translate", async (request, response)=>{
+    // word to from
+   // const word = request.query.word;
+ //   const from = request.query.from;
+   // const to = request.query.to;
+
+    const { word, from, to } = request.query;
+
+    const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+    const res = await axios.get(API);
+
+const wrangleData = {
+    translation: res.data.responseData.translatedText,
+    match
+}
+
+    response.json(res.data.responseData.translatedText);
+})
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
