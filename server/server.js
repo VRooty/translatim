017c212af55cfda7513882961f3a676923eeddc1
Vrooty <victoriadohertybone@live.co.uk>
2023-11-02 10:38:35 +0000

    my message

--- server/server.js
+++ server/server.js
@@ -3,10 +3,9 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(cors());
-const axios = require("axios")
-// add your endpoints here
+const axios = require("axios");
 
-app.get(PORT, (_, RESPONSE) => RESPONSE.JSON("Root route for translatim"))
+
+app.get("/", (_, response) => response.json("Root route for translatim."));a
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
